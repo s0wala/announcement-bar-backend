26c1519457670694a83aed005775f1342227770c
Sonia Battaglia <jenniferadams@sonias-air.lan>
2025-12-01 09:15:06 -0500

    Fix CORS credentials

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
     origin: '*',
-    credentials: true
+    credentials: false
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
